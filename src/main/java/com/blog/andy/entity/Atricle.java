b291f36091606f5909efc4914e5811ad5f6fd759
Andy <promise2mm@163.com>
2015-09-09 21:01:24 +0800

    first commit

---
+++ src/main/java/com/blog/andy/entity/Atricle.java
@@ -0,0 +1,11 @@
+package com.blog.andy.entity;
+
+/**
+ * Created by promi on 2015/9/9.
+ */
+public class Atricle {
+    public static void main(String[] args){
+        System.out.println("Hello, Andy!");
+    }
+}
+git branch --set-upstream master origin/master
